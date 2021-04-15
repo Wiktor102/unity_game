a00484cc7232d52bcf5c0b9be61908b00a874570
Wiktor102 <wiktorgolicz1@gmail.com>
2021-04-15 18:22:21 +0200

    Before lesson 23

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float fallMultiplier;
     public float lowJumpFallMultiplier;
-    public float groundCheckRange = 0.1f;
+    public float groundCheckRange;
     public int Health;
     public bool IsHurt;
     bool jumpPressed;
@@ -24,107 +24,91 @@ public class PlayerController : MonoBehaviour
     private Animator _animator;
     private SpriteRenderer _renderer;
     private bool groundedMemory = true;
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    void Start () {
         fallMultiplier = 1.2f;
         lowJumpFallMultiplier = 1.1f;
-        playerSpeed = 150f;
-        _renderer = GetComponentInParent<SpriteRenderer>();
-        _animator = GetComponentInParent<Animator>();
+        playerSpeed = 5f;
+        verticalPlayerSpeed = 5f;
+        groundCheckRange = 0.1f;
 
         Health = 100;
         IsHurt = false;
+
+        _renderer = GetComponentInParent<SpriteRenderer>();
+        _animator = GetComponentInParent<Animator>();
+
         _healthBarController = HealthBar.GetComponent<SliderController>();
         _healthBarController.Set(Health);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update () {
         CheckInput();
         BetterFall();
         MovePlayer();
     }
 
-    void CheckInput()
-    {
+    void CheckInput () {
         horizontalMovement = Input.GetAxisRaw("Horizontal");
         jumpPressed = Input.GetKeyDown(KeyCode.Space);
         SetAnimation();
     }
 
-    public void GotHit(int damage){
+    public void GotHit (int damage){
         Health -= Math.Abs(damage);
         IsHurt = true;
         _healthBarController.Set(Health);
         _animator.SetBool("hurt", true);
     }
 
-
-
-    void MovePlayer()
-    {
+    void MovePlayer () {
         var yMovement = playerBody.velocity.y;
 
-        if (jumpPressed)
-        {
-            Debug.Log("jump pressed!");
+        if (jumpPressed)  {
+            // Debug.Log("jump pressed!");
             yMovement += jumpForce;
-
-                _animator.SetBool("jumping", true);
-                Debug.Log("Setting jumping to true");
-        
+            _animator.SetBool("jumping", true);
+            // Debug.Log("Setting jumping to true");
         }
 
-        if (!groundedMemory && IsGrounded())
-        {
+        if (!groundedMemory && IsGrounded()) {
             _animator.SetBool("jumping", false);
             Debug.Log("Setting jumping to false");
         }
-        _animator.SetBool("grounded", IsGrounded());
 
+        _animator.SetBool("grounded", IsGrounded());
         _animator.SetFloat("vertical_speed", yMovement);
         _animator.SetFloat("speed", Math.Abs(horizontalMovement));
-        var playerPosition = horizontalMovement * playerSpeed * Time.deltaTime;
+
+        var playerPosition = horizontalMovement * playerSpeed;
         playerBody.velocity = new Vector2(playerPosition, yMovement);
+
         groundedMemory = IsGrounded();
     }
-    public void IsClimbing(bool isClimbing)
-    {
+
+    public void IsClimbing (bool isClimbing) {
         _animator.SetBool("climbing", isClimbing);
         Debug.Log($"setting climbing flag to {isClimbing}");
     }
 
 
-    void BetterFall()
-    {
-        // czy nasz player spada?
-        if (playerBody.velocity.y < 0 && enableShortJump)
-        {
-            //fallMultiplier
+    void BetterFall () {
+        if (playerBody.velocity.y < 0 && enableShortJump) {
             playerBody.velocity += Vector2.up * Physics2D.gravity * fallMultiplier * Time.deltaTime;
-        }
-        // player leci do góry, ale spacja nie jest wciśnięta
-        else if (playerBody.velocity.y > 0 && !jumpPressed)
-        {
+        } else if (playerBody.velocity.y > 0 && !jumpPressed) {
+            // player leci do góry, ale spacja nie jest wciśnięta
             playerBody.velocity += Vector2.up * Physics2D.gravity * lowJumpFallMultiplier * Time.deltaTime;
-            //lowJumpFallMultiplier
         }
     }
-    void OnCollisionExit2D(Collision2D col)
-    {
+    void OnCollisionExit2D(Collision2D col) {
         if(col.gameObject.name == "Frog"){
             IsHurt = false;
             _animator.SetBool("hurt", false);
         }
     }
-    private void SetAnimation()
-    {
+    private void SetAnimation () {
         _renderer.flipX = horizontalMovement < 0;
         _animator.SetFloat("speed", Math.Abs(horizontalMovement));
     }
     private bool IsGrounded() => Physics2D.OverlapCircle(groundChecker.position, groundCheckRange, groundLayer) != null;
-
-
 }
